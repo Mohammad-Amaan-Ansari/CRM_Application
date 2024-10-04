2e513110ee696c0f225ca864e48b3fc019f01285
Mohammad Amaan Ansari <amaan2003am@gmail.com>
2024-10-04 19:44:10 +0530

    product API

--- db/migrate/20240903063552_create_orders.rb
+++ db/migrate/20240903063552_create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
       t.string :order_no
-      t.decimal :total_amount
+      t.float :total_amount
       t.string :invoice
 
       t.timestamps
